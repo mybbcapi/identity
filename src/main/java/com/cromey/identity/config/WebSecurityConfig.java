cfd647648e489702dd42aede4521740addff603d
Paul Cromey <paul.cromey@gmail.com>
2021-01-17 10:26:55 +0000

    add csrf disable for test demo

---
+++ src/main/java/com/cromey/identity/config/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.cromey.identity.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // ruleid: spring-csrf-disabled
+        http
+            .csrf()
+            .disable();
+    }
+    
+}
