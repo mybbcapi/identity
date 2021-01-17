ba60bcb76a83862472487b9ad679583c6b806f72
Paul Cromey <paul.cromey@gmail.com>
2021-01-17 10:30:32 +0000

    test

--- src/main/java/com/cromey/identity/config/WebSecurityConfig.java
+++ src/main/java/com/cromey/identity/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // ruleid: spring-csrf-disabled
+        // r/java.spring.security.audit.spring-csrf-disabled.spring-csrf-disabled
         http
             .csrf()
             .disable();
